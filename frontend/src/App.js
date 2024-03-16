b97f88b0e9ba3cbe0c23340c9b3a558cc220af7d
Kashish <kashyapkashish22@gmail.com>
2024-03-16 21:49:53 +0530

    deployment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,25 +10,30 @@ import ViewPost from './Components/Posts/ViewPost';
 import UpdatePost from './Components/Posts/UpdatePost';
 import DeletePost from './Components/Posts/DeletePost';
 
-// const PrivateRoute = ({ element, ...rest }) => {
-//   const isAuthenticated = !!localStorage.getItem('token');
-//   return isAuthenticated ? element : <Navigate to="/login" />;
-// };
+const PrivateRoute = ({ element, ...rest }) => {
+  const isAuthenticated = !!localStorage.getItem('token');
+  return isAuthenticated ? element : <Navigate to="/login" />;
+};
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/view" element={<ViewUser />} />
+        {/* <Route path="/view" element={<ViewUser />} />
         <Route path="/view/update" element={<UpdateUser />} />
         <Route path="/view/delete" element={<DeleteUser />} />
         <Route path="/post/create" element={<CreatePost />} />
         <Route path="/post/view/:id" element={<ViewPost />} />
         <Route path="/post/update/:id" element={<UpdatePost />} />
-        <Route path="/post/delete/:id" element={<DeletePost />} />
-        {/* <Route path="/products" element={<PrivateRoute element={<Products />} />} />
-        <Route path="/" element={<PrivateRoute element={<Navigate to="/products" />} />} /> */}
+        <Route path="/post/delete/:id" element={<DeletePost />} /> */}
+        <Route path="/view" element={<PrivateRoute element={<ViewUser />} />} />
+        <Route path="/view/update" element={<PrivateRoute element={<UpdateUser />} />} />
+        <Route path="/view/delete" element={<PrivateRoute element={<DeleteUser />} />} />
+        <Route path="/post/create" element={<PrivateRoute element={<CreatePost />} />} />
+        <Route path="/post/view/:id" element={<PrivateRoute element={<ViewPost />} />} />
+        <Route path="/post/update/:id" element={<PrivateRoute element={<UpdatePost />} />} />
+        <Route path="/post/delete/:id" element={<PrivateRoute element={<DeletePost />} />} />
       </Routes>
     </Router>
   );
